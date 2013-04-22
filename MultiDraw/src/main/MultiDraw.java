11b5ef53c1845c7330374a3db8f9b3745ea8b5dd
Jonas Holmström <jholmstroem@gmail.com>
2013-04-23 00:45:27 +0200

    Rename main class

--- MultiDraw/src/main/MultiDraw.java
+++ MultiDraw/src/main/MultiDraw.java
@@ -5,18 +5,12 @@ import javax.swing.JFrame;
 import javax.swing.UIManager;
 import javax.swing.UnsupportedLookAndFeelException;
 
-class AOJA {
+class MultiDraw {
 	public static void main(String[] args) {
 		try {
 			// Fix look and feel of GUI.
 			UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
-		} catch (ClassNotFoundException e) {
-			e.printStackTrace();
-		} catch (InstantiationException e) {
-			e.printStackTrace();
-		} catch (IllegalAccessException e) {
-			e.printStackTrace();
-		} catch (UnsupportedLookAndFeelException e) {
+		} catch (Exception e) {
 			e.printStackTrace();
 		}
 		PaintWindow window = new PaintWindow();
