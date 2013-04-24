5f0d5f49d8ff4bf205b3d119c198f0a60e49f48b
Adam <ada09an1@student.lth.se>
2013-04-24 12:44:03 +0200

    commiting useless pointless insignificant non-change

--- MultiDraw/src/spike/mane.java
+++ MultiDraw/src/spike/mane.java
@@ -31,7 +31,7 @@ class Inputer extends Thread{ // två sådana här running at the same time ger 
 	}
 
 	public void run(){
-		System.out.println("TYPE LATER!");
+		System.out.println("TYPIng LATER!");
 		String s = "";
 		
 		while((s = scan.next()) != null){
