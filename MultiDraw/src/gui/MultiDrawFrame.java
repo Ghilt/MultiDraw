5d64acb90ebbda60e6af40b88ccaaafb393dd3b0
Adam <ada09an1@student.lth.se>
2013-04-25 14:34:44 +0200

    refaktoriserade multidrawframe, bröt ut i lite metoder

--- MultiDraw/src/gui/MultiDrawFrame.java
+++ MultiDraw/src/gui/MultiDrawFrame.java
@@ -1,4 +1,5 @@
 package gui;
+
 import java.awt.BorderLayout;
 import java.awt.Dimension;
 
@@ -11,53 +12,18 @@ import javax.swing.JPanel;
 import javax.swing.JScrollPane;
 
 public class MultiDrawFrame extends JFrame {
-	
-	/**
-	 * Blabla
-	 */
+
 	public MultiDrawFrame(PaintPanel paintpanel) {
 		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		this.setTitle("AOJA POWER ARTIST CANVAS EDITOR PRO");
-		
-		JPanel leftPanel = new JPanel();
-		leftPanel.setPreferredSize(new Dimension(80, 800));
-		leftPanel.add(new JLabel("Tools"));
-		
-		// Color button
-		ColorButton colorButton = new ColorButton("color", paintpanel);
-		leftPanel.add(colorButton);
-		
-		// Brush size slider
-		BrushSizeSlider brushSizeSlider = new BrushSizeSlider(paintpanel);
-		leftPanel.add(brushSizeSlider);
-		
-		// Draw line button
-		DrawLineButton drawLineButton = new DrawLineButton("Line", paintpanel);
-		leftPanel.add(drawLineButton);
-		
-		JPanel rightPanel = new JPanel();
-		rightPanel.setPreferredSize(new Dimension(150, 800));
-		rightPanel.add(new JLabel("Users"));
 
-		JScrollPane scroller = new JScrollPane(paintpanel);
-		scroller.setBorder(BorderFactory.createEmptyBorder());
-		
-		JPanel chatPanel = new JPanel();
-		chatPanel.add(new JLabel("Chat"));
-		chatPanel.setPreferredSize(new Dimension(800, 150));
-		
-		JPanel centerPanel = new JPanel();
-		centerPanel.setLayout(new BorderLayout());
-		centerPanel.add(scroller, BorderLayout.CENTER);
-		centerPanel.add(chatPanel, BorderLayout.SOUTH);
-		
-		JMenuBar menuBar = new JMenuBar();
-		JMenu fileItem = new JMenu("File");
-		JMenu editItem = new JMenu("Edit");
-		JMenu aboutItem = new JMenu("About");
-		menuBar.add(fileItem);
-		menuBar.add(editItem);
-		menuBar.add(aboutItem);
+		JPanel leftPanel = makeLeftPanel(paintpanel);
+
+		JPanel rightPanel = makeRightPanel(paintpanel);
+
+		JPanel centerPanel = makeCenterPanel(paintpanel);
+
+		JMenuBar menuBar = makeMenuBar();
 
 		this.setLayout(new BorderLayout());
 		this.setJMenuBar(menuBar);
@@ -67,5 +33,60 @@ public class MultiDrawFrame extends JFrame {
 
 		this.pack();
 		this.setVisible(true);
+	}
+
+	private JMenuBar makeMenuBar() {
+		JMenuBar menuBar = new JMenuBar();
+		JMenu fileItem = new JMenu("File");
+		JMenu editItem = new JMenu("Edit");
+		JMenu aboutItem = new JMenu("About");
+		menuBar.add(fileItem);
+		menuBar.add(editItem);
+		menuBar.add(aboutItem);
+		return menuBar;
+	}
+
+	private JPanel makeCenterPanel(PaintPanel paintpanel) {
+
+		JScrollPane scroller = new JScrollPane(paintpanel);
+		scroller.setBorder(BorderFactory.createEmptyBorder());
+
+		JPanel chatPanel = new JPanel();
+		chatPanel.add(new JLabel("Chat"));
+		chatPanel.setPreferredSize(new Dimension(800, 150));
+
+		JPanel centerPanel = new JPanel();
+		centerPanel.setLayout(new BorderLayout());
+		centerPanel.add(scroller, BorderLayout.CENTER);
+		centerPanel.add(chatPanel, BorderLayout.SOUTH);
+
+		return centerPanel;
+	}
+
+	private JPanel makeRightPanel(PaintPanel paintpanel) {
+		JPanel rightPanel = new JPanel();
+		rightPanel.setPreferredSize(new Dimension(150, 800));
+		rightPanel.add(new JLabel("Users"));
+		return rightPanel;
+	}
+
+	private JPanel makeLeftPanel(PaintPanel paintpanel) {
+		JPanel leftPanel = new JPanel();
+		leftPanel.setPreferredSize(new Dimension(80, 800));
+		leftPanel.add(new JLabel("Tools"));
+
+		// Color button
+		ColorButton colorButton = new ColorButton("color", paintpanel);
+		leftPanel.add(colorButton);
+
+		// Brush size slider
+		BrushSizeSlider brushSizeSlider = new BrushSizeSlider(paintpanel);
+		leftPanel.add(brushSizeSlider);
+
+		// Draw line button
+		DrawLineButton drawLineButton = new DrawLineButton("Line", paintpanel);
+		leftPanel.add(drawLineButton);
+		return leftPanel;
+
 	}
 }
