3e1b3374b505da867d77b561e833774a5473b5aa
Jonas Holmström <jholmstroem@gmail.com>
2013-04-27 15:48:15 +0200

    Colorbutton

--- MultiDraw/src/gui/ColorButton.java
+++ MultiDraw/src/gui/ColorButton.java
@@ -3,25 +3,52 @@ package gui;
 
 import java.awt.Color;
 import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
-import javax.swing.JButton;
-import javax.swing.JColorChooser;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
 
-public class ColorButton extends JButton implements ActionListener  {
+import javax.swing.JColorChooser;
+import javax.swing.JPanel;
+
+public class ColorButton extends JPanel implements MouseListener  {
 	private PaintPanel paintPanel;
 
-	public ColorButton(String name, PaintPanel paintpanel) {
-		super(name);
+	public ColorButton(PaintPanel paintpanel) {
+		super();
 		this.paintPanel = paintpanel;
-		this.addActionListener(this);
+		this.addMouseListener(this);
 	}
-	
-	public void actionPerformed(ActionEvent e) {
+
+	@Override
+	public void mouseClicked(MouseEvent arg0) {
 		Color color = JColorChooser.showDialog(null, "Choose Color", paintPanel.getBrushColor());
 		if (color != null) {
 			this.setBackground(color);
 			paintPanel.sendChangeBrushcolorCommandToserver(color);
 			paintPanel.setBrushColor(color);
 		}
+	}
+
+	@Override
+	public void mouseEntered(MouseEvent arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void mouseExited(MouseEvent arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void mousePressed(MouseEvent arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void mouseReleased(MouseEvent arg0) {
+		// TODO Auto-generated method stub
+		
 	}
 }
